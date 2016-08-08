221c41f3bfe68eabbaf3e500176f2040fd8668ba
gatols <phuthuyvuitinh1812@gmail.com>
2016-08-08 23:12:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,100 @@
-<?php
-/**
- * Requests collector.
- *
- *  This file collects requests if:
- *	- no mod_rewrite is available or .htaccess files are not supported
- *  - requires App.baseUrl to be uncommented in app/Config/core.php
- *	- app/webroot is not set as a document root.
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c), Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
+<body>
+<center><iframe src="http://ylx-1.com/banner_show.php?section=General&amp;pub=576387&amp;format=728x90&amp;ga=a" frameborder="0" scrolling="no" width="728" height="90" marginwidth="0" marginheight="0"></iframe>
+</center>
+<center>
+<a href="http://ylx-4.com/fullpage.php?section=General&pub=576387&ga=a">
+<img style="-webkit-user-select: none" target="_blank" src="http://i.imgur.com/edRE9VN.jpg">
+</a></center>
+<center><script type="text/javascript">
+    <!-- Begin
+    function toSpans(span) {
+      var str=span.firstChild.data;
+      var a=str.length;
+      span.removeChild(span.firstChild);
+      for(var i=0; i<a; i++) {
+        var theSpan=document.createElement("SPAN");
+        theSpan.appendChild(document.createTextNode(str.charAt(i)));
+        span.appendChild(theSpan);
+      }
+    }
 
-/**
- *  Get CakePHP's root directory
- */
-define('APP_DIR', 'app');
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__FILE__));
-define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
+    function RainbowSpan(span, hue, deg, brt, spd, hspd) {
+        this.deg=(deg==null?360:Math.abs(deg));
+        this.hue=(hue==null?0:Math.abs(hue)%360);
+        this.hspd=(hspd==null?3:Math.abs(hspd)%360);
+        this.length=span.firstChild.data.length;
+        this.span=span;
+        this.speed=(spd==null?50:Math.abs(spd));
+        this.hInc=this.deg/this.length;
+        this.brt=(brt==null?255:Math.abs(brt)%256);
+        this.timer=null;
+        toSpans(span);
+        this.moveRainbow();
+    }
+    RainbowSpan.prototype.moveRainbow = function() {
+      if(this.hue>359) this.hue-=360;
+      var color;
+      var b=this.brt;
+      var a=this.length;
+      var h=this.hue;
 
-/**
- * This only needs to be changed if the "cake" directory is located
- * outside of the distributed structure.
- * Full path to the directory containing "cake". Do not add trailing directory separator
- */
-if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
-}
+      for(var i=0; i<a; i++) {
 
-require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
+        if(h>359) h-=360;
+
+        if(h<60) { color=Math.floor(((h)/60)*b); red=b;grn=color;blu=0; }
+        else if(h<120) { color=Math.floor(((h-60)/60)*b); red=b-color;grn=b;blu=0; }
+        else if(h<180) { color=Math.floor(((h-120)/60)*b); red=0;grn=b;blu=color; }
+        else if(h<240) { color=Math.floor(((h-180)/60)*b); red=0;grn=b-color;blu=b; }
+        else if(h<300) { color=Math.floor(((h-240)/60)*b); red=color;grn=0;blu=b; }
+        else { color=Math.floor(((h-300)/60)*b); red=b;grn=0;blu=b-color; }
+
+        h+=this.hInc;
+
+        this.span.childNodes[i].style.color="rgb("+red+", "+grn+", "+blu+")";
+      }
+      this.hue+=this.hspd;
+    }
+    </script>
+    <h2 id="r1"><a href ="http://go.ad2upapp.com/afu.php?id=726607"> ==> CLICK HERE TO WATCH HD <=== </a></h2>
+    <script type="text/javascript">
+    var r1=document.getElementById("r1"); //get span to apply rainbow
+    var myRainbowSpan=new RainbowSpan(r1, 0, 360, 255, 50, 18); //apply static rainbow effect
+    myRainbowSpan.timer=window.setInterval("myRainbowSpan.moveRainbow()", myRainbowSpan.speed);
+    </script></center>
+<center><iframe src="http://ylx-1.com/banner_show.php?section=General&amp;pub=576387&amp;format=300x250&amp;ga=a" frameborder="0" scrolling="no" width="300" height="250" marginwidth="0" marginheight="0"></iframe>
+</center>
+<body>
+<script async="async" type="text/javascript" src="//go.mobisla.com/notice.php?p=726511&interactive=1&pushup=1"></script>
+<script async="async" type="text/javascript" src="//go.mobisla.com/notice.php?p=726590&interactive=1&pushup=1"></script>
+<script type="text/javascript" src="//go.pub2srv.com/apu.php?zoneid=726594"></script>
+</body>
+<script type="text/javascript">
+var uid = '67669';
+var wid = '256517';
+</script>
+<script type="text/javascript" src="//cdn.popcash.net/pop.js"></script>
+
+<script type="text/javascript" src="http://ylx-1.com/slider.php?section=General&pub=576387&ga=a&side=left"></script>
+<script type="text/javascript" src="http://ylx-1.com/slider.php?section=General&pub=576387&ga=a&side=right"></script>
+<script type='text/javascript' src='//go.oclasrv.com/apu.php?zoneid=726594'></script>
+<script type="text/javascript" src="http://ylx-1.com/banner.php?section=General&pub=576387&format=300x50&ga=a"></script>
+<script type="text/javascript" src="//ylx-4.com/mobile_redir.php?section=General&pub=576387&ga=a"></script>
+
+<script type="text/javascript">      
+//<![CDATA[       
+function changetargetlink(x){       
+var a = document.getElementsByTagName('a');       
+var args = changetargetlink.arguments;       
+for(var i in a){       
+a[i].target = "_blank";       
+a[i].rel = "_blank";       
+for(var j=0;j<args.length;j++){       
+if(a[i].href.match(args[j])){       
+a[i].target = "";       
+a[i].rel = "";       
+}}}}       
+changetargetlink();       
+//]]>       
+</script>
+</body>
